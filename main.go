a9ea5ee2d7168a12856c9b5ca506633f45878c56
Yota Toyama <raviqqe@gmail.com>
2017-11-17 23:56:29 +0900

    Parse arguments

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	docopt "github.com/docopt/docopt-go"
+)
+
 func main() {
+	getArgs()
+}
+
+func getArgs() map[string]interface{} {
+	usage := `Link checker for Markdown and HTML
+
+Usage:
+	linkcheck <filename>`
+
+	args, err := docopt.Parse(usage, nil, true, "linkcheck", true)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	return args
 }
