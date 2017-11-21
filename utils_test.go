90b0c0dcf72496ccfa51ef78b77227ed872ab88b
Yota Toyama <raviqqe@gmail.com>
2017-11-21 21:39:21 +0900

    Test utils.go

--- utils_test.go
+++ utils_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringSetToSlice(t *testing.T) {
+	assert.Equal(t, []string{"foo", "bar"}, stringSetToSlice(map[string]bool{"foo": true, "bar": false}))
+}
+
+func TestIndent(t *testing.T) {
+	for _, c := range []struct {
+		source, target string
+	}{
+		{"foo", "\tfoo"},
+		{"foo\nbar", "\tfoo\n\tbar"},
+	} {
+		assert.Equal(t, c.target, indent(c.source))
+	}
+}
